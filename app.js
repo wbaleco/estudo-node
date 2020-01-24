c0cd42a310c38362156db6a04347b1ab879b383a
Walter Baleco <Wbaleco@hotmail.com>
2020-01-24 17:05:38 -0300

    HANDLEBARS - Criar formulário e cadastrar no MYSQL

--- app.js
+++ app.js
@@ -2,9 +2,14 @@ const express = require('express');
 const app = express();
 
 const handlebars = require('express-handlebars');
+const bodyParser = require('body-parser');
 
 app.engine('handlebars', handlebars({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
+
+//Configuração body-parser
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 //Rotas
 
@@ -18,6 +23,10 @@ app.get('/add-pagamento', (req, res) => {
 
 app.get('/contato', (req, res) => {
     res.render("contato");
+});
+
+app.post('/cad-pagamento', (req, res) => {
+    res.send("Nome: " + req.body.nome + "Valor: " + req.body.valor);
 });
 
 /* const sequelize = new Sequelize('gerenciadorfinanceiro', 'wbaleco', 'B@rB@r@1997', {
