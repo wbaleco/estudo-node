e23b3769752d6efc6c04eb517406c831894a7469
Walter Baleco <Wbaleco@hotmail.com>
2020-01-24 13:24:09 -0300

    HANDLEBARS - Definir layout padrão, criar arquivos e rotas

--- app.js
+++ app.js
@@ -1,22 +1,26 @@
 const express = require('express');
 const app = express();
-const mysql = require('mysql');
-const Sequelize = require('sequelize');
-const DataTypes = require('sequelize');
 
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + "/src/home.html");
+const handlebars = require('express-handlebars');
+
+app.engine('handlebars', handlebars({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
+//Rotas
+
+app.get('/listar-pagamento', (req, res) => {
+    res.render('listar-pagamento');
 });
 
-app.get('/sobre', (req, res) => {
-    res.sendFile(__dirname + "/src/sobre.html");
+app.get('/add-pagamento', (req, res) => {
+    res.render('add-pagamento');
 });
 
 app.get('/contato', (req, res) => {
-    res.sendFile(__dirname + "/src/contato.html");
+    res.render("contato");
 });
 
-const sequelize = new Sequelize('gerenciadorfinanceiro', 'wbaleco', 'B@rB@r@1997', {
+/* const sequelize = new Sequelize('gerenciadorfinanceiro', 'wbaleco', 'B@rB@r@1997', {
     host: 'localhost',
     dialect: 'mysql'
 });
@@ -27,81 +31,8 @@ sequelize.authenticate().then(function(err) {
 }).catch(function(error) {
     console.error('Erro ao tentar conectar com banco:', error);
 });
-
-const Pagamento = sequelize.define('pagamentos', {
-    // Model attributes are defined here
-    nome: {
-        type: DataTypes.STRING,
-
-    },
-    valor: {
-        type: DataTypes.DOUBLE
-
-    }
-
-});
-
-Pagamento.create({
-    nome: "Energia",
-    valor: "350"
-});
-//Esta linha quando executada cria a tabela
-//Pagamento.sync({ force: true });
-
-
-/* const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'wbaleco',
-    password: 'B@rB@r@1997',
-    database: 'gerenciadorfinanceiro'
-});
-
-connection.connect(function(err) {
-    if (err) {
-        console.error('error connecting: ' + err.stack);
-        return;
-    }
-
-    console.log('connected as id ' + connection.threadId);
-}); */
-
-//SELECT
-/* connection.query('SELECT * FROM users', function(err, rows, fields) {
-    if (!err) {
-        console.log("Resultado :", rows);
-    } else {
-        console.log("Erro ao pesquisar");
-
-    }
-
-});
-//INSERT    
-connection.query("INSERT INTO users(nome, email) VALUES ('Walter', 'wbaleco@gmail.com')", function(err, result) {
-
-
-    if (!err) {
-        console.log("Cadastro realizado com sucesso");
-    } else {
-        console.log("Erro ao tentar cadastrar");
-    }
-});
-
-connection.query("UPDATE users set nome = 'Baleco' where id = 1", function(err, result) {
-    if (!err) {
-        console.log("Alteração realizada com sucesso");
-    } else {
-        console.log("Erro ao tentar alterar");
-    }
-});
-
-connection.query("DELETE FROM users where id = 2", (err, result) => {
-    if (!err) {
-        console.log("Usuário excluido com sucesso");
-    } else {
-        console.log("Erro ao tentar excluir usuário");
-    }
-});
  */
+
 app.listen(3000, () => {
     console.log(`Server started on port`);
 });
